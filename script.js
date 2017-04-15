fd47994335c43f9ecf5f125752cc98fe4fb4fc4e
rafalwawrzyk <rafal.wawrzyk@gmail.com>
2017-04-15 16:34:40 +0200

    finished the scripts
    add scripts

--- script.js
+++ script.js
@@ -1,7 +1,50 @@
 $(function(){
     
     
-    console.log('hello');
+   var checkOut = $('.action-message');
+    var closeBtn = $('.close-btn');
+    var modal = $('.modal');
+    
+    
+    checkOut.on({
+        click: function(){
+           openModal();
+        }
+    })
+    
+    closeBtn.on({
+        click: function(){
+            closeModal();
+        }
+    })
+    
+    modal.on({
+        click: function(event){
+            if(event.target == this){
+                closeModal();
+            }
+        }
+    })
+    
+    
+    
+    function openModal(){
+        modal.css({
+            visibility: "visible",
+            opacity:0.0
+        }).animate({
+            opacity:1.0
+        },500);
+    };
+    
+    function closeModal(){
+        modal.css({
+            visibility:"hidden",
+            opacity:1.0
+        }).animate({
+            opacity:0.0
+        })
+    }
     
     
     
@@ -10,9 +53,15 @@ $(function(){
     
     
     
+    });
+    
+    
+    
+    
+   
     
     
     
     
     
-})
+    
